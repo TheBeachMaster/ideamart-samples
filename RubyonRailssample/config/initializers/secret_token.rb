784d4e9d5a0de41c7d23d45a1f2a2dd462e5327e
dialogpsi <devsupport@ideamart.lk>
2013-01-28 15:14:26 +0530

    initial upload

---
+++ RubyonRailssample/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shuttleussd::Application.config.secret_token = '3327dce19eb0167c7c0f1e6e066560394b320abf8fe115981a96455dd17d90cf7ac11676a2eed7ac96a9ed89cad9052818a35932b405dacec82dcb72c68bc85f'
